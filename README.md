cce94ff82737a8f00ef31e941a54b15aefe07f4b
Arryan Bhatnagar <bhatnag8@purdue.edu>
2023-11-22 23:55:23 -0500

    working POST with content/url

--- README.md
+++ README.md
@@ -1,3 +1,19 @@
-project phase 2
+# A Trustworthy Module Registry
 
-ECE 461 Group 8
+Fall 2023 ECE 461 Group 8. Project Phase 2.
+
+
+### Installation
+
+Step-by-step instructions on how to install and run the project.
+
+```bash
+# Command to install dependencies
+./run install
+```
+
+```js
+# Starts the program
+npm run API
+```
+
--- rest_api/routes/post_package.ts
+++ rest_api/routes/post_package.ts
@@ -1,14 +1,14 @@
-
 import { Router, Request, Response } from 'express';
 import { promisify } from 'util';
 import { exec as execCallback } from 'child_process';
 import { promises as fs } from 'fs';
 import * as fileSystem from 'fs';
 import * as archiver from 'archiver';
-import * as path from 'path';
+import * as pathModule from 'path';
+import * as AdmZip from 'adm-zip'; // Import the 'adm-zip' module
 
 import * as schema from '../schema';
-import { connectToDatabase } from "../db";
+import { connectToDatabase, dbName, tableName } from "../db";
 
 /**
 USE packages;
@@ -19,14 +19,19 @@ SELECT * FROM packages;
 
 const router = Router();
 const exec = promisify(execCallback);
-const readFileAsync =
-promisify(fileSystem.readFile);
+const readFileAsync = promisify(fileSystem.readFile);
+let table = `${dbName}.${tableName}`
 
 router.post('/', async (req: Request, res: Response) => {
     const { Content, JSProgram, URL } = req.body as schema.PackageData;
-    if (fileSystem.existsSync('rest_api/dump')) {
-        fileSystem.rmSync('rest_api/dump', { recursive: true });
-    }
+
+    /** delete 'dump' directory if it exists **/
+    await fs.rm('rest_api/dump', { recursive: true, force: true }).catch(err => {
+        if (err.code !== 'ENOENT') {
+            throw err;
+        }
+    });
+
     /** checks if only either Content or URL is being passsed when inserting a package **/
     if (!((Content && JSProgram && !URL) || (URL && JSProgram && !Content))) {
         return res.status(400).json({
@@ -44,45 +49,46 @@ router.post('/', async (req: Request, res: Response) => {
         });
     }
 
-    /** get the number of packages from the database to set the ID **/
-    let packagesCount: number;
-    try {
-        const [results] = await connection.execute(
-            'SELECT COUNT(*) as rowCount FROM packages'
-            );
-        packagesCount = results[0].rowCount;
-    } catch (error) {
-        return res.status(503).json({
-            error: `Error connecting to the database: ${error}`,
-        });
-    }
-
     let name: string = '';
     let version: string = '';
-    let id: number = packagesCount + 1;
 
     /** download the repo from the link and extract information **/
     if (URL) {
         try {
-            const packageJsonPath = await downloadRepo(URL);
-            const packageJsonContent = await fs.readFile(packageJsonPath, 'utf-8');
-            const packageJson = JSON.parse(packageJsonContent);
-            name = packageJson.name;
-            version = packageJson.version;
+            await downloadRepo(URL, 'rest_api/dump');
         } catch (error) {
             return res.status(503).json({
-                error: `Failed to open the package.json file and/or package.json doesn't contain name/version. Error: ${error}`,
+                error: `Invalid GitHub Link. ${error}`,
             });
         }
-    } else if (Content) {
 
+    } else if (Content) {
+        try {
+            let zipPath = await decodeBase64AndExtract(Content, 'rest_api/dump');
+        } catch (error) {
+            return res.status(503).json({
+                error: `Couldn't read zip file. ${error}`,
+            });
+        }
+    }
+
+    try {
+        const packageJsonPath = pathModule.join('rest_api/dump', 'package.json')
+        const packageJsonContent = await fs.readFile(packageJsonPath, 'utf-8');
+        const packageJson = JSON.parse(packageJsonContent);
+        name = packageJson.name;
+        version = packageJson.version;
+    } catch (error) {
+        return res.status(503).json({
+            error: `Failed to open the package.json file and/or package.json doesn't contain name/version. ${error}`,
+        });
     }
 
     /** check if package already exists in the database **/
     let packageExists;
     try {
         const [existingRows] = await connection.execute(
-            'SELECT * FROM packages WHERE name = ? AND version = ?', [name, version]
+            `SELECT * FROM ${table} WHERE name = ? AND version = ?`, [name, version]
             );
         packageExists = existingRows.length > 0;
 
@@ -108,9 +114,93 @@ router.post('/', async (req: Request, res: Response) => {
     **/
 
     /** create the zip file **/
-    const sourceDirectory = path.join(__dirname, '../dump');
+    const zipResult = await ZIP('../dump', name, version, 'rest_api', res);
+    const { fileContent, outputZipPath }: { fileContent: string, outputZipPath: string } = zipResult;
+
+    /** database entry **/
+    let responseData : schema.Package;
+    if (URL) {
+        try {
+            const [results] = await connection.execute(
+                `INSERT INTO ${table} (Name, Version, URL, JSProgram, Content) VALUES (?, ?, ?, ?, ?)`
+                , [name, version, URL, JSProgram, fileContent]);
+            responseData = {
+                metadata: { Name: name, Version: version, ID: '`' },
+                data: { URL, JSProgram }
+            };
+
+        } catch (error) {
+            return res.status(500).json({
+                error: `Failed to insert URL to the database. Please try again later. ${error}`
+            });
+        }
+
+    } else if (Content) {
+        try {
+            const [results] = await connection.execute(
+                `INSERT INTO ${table} (Name, Version, JSProgram, Content) VALUES (?, ?, ?, ?)`
+                , [name, version, JSProgram, fileContent]);
+            responseData = {
+                metadata: { Name: name, Version: version, ID: '`' },
+                data: { URL, JSProgram }
+            };
+
+        } catch (error) {
+            return res.status(500).json({
+                error: `Failed to insert URL to the database. Please try again later. ${error}`
+            });
+        }
+    }
+
+    /** delete 'dump' directory if it exists **/
+    await fs.rm('rest_api/dump', { recursive: true, force: true }).catch(err => {
+        if (err.code !== 'ENOENT') {
+            throw err;
+        }
+    });
+
+    if (fileSystem.existsSync(outputZipPath)) {
+        fileSystem.rmSync(outputZipPath, { recursive: true });
+    }
+
+    await connection.end();
+    return res.status(201).json(responseData);
+});
+
+export async function downloadRepo(url: string, path: string)  {
+
+    /** delete 'dump' directory if it exists **/
+    await fs.rm(path, { recursive: true, force: true }).catch(err => {
+        if (err.code !== 'ENOENT') {
+            throw err;
+        }
+    });
+
+    /** create 'dump' directory if it doesn't exist **/
+    await fs.mkdir(path).catch(err => {
+        if (err.code !== 'EEXIST') {
+            throw err;
+        }
+    });
+
+    let failed = 'The Repository does not exist or you do not have permission to access it.';
+    try {
+        await Promise.race([
+            exec(`git clone ${url} ${path}`),
+            new Promise((_, reject) => setTimeout(() => reject(new Error(failed)), 60000))
+        ]);
+    } catch (error) {
+        throw error;
+    }
+
+    /** return path of 'dump' **/
+    return path;
+}
+
+export async function ZIP(sourcePath: string, name: string, version: string, filePath: string, res) {
+    const sourceDirectory = pathModule.join(__dirname, sourcePath);
     const zipFileName = `${name} [${version}].zip`;
-    const outputZipPath = `rest_api/${zipFileName}`;
+    const outputZipPath: string = `${filePath}/${zipFileName}`;
     const outputZipStream = fileSystem.createWriteStream(outputZipPath);
     const archive = archiver('zip', {
         zlib: { level: 9 },
@@ -135,56 +225,41 @@ router.post('/', async (req: Request, res: Response) => {
 
     archive.finalize();
     await fileContentPromise;
-    const fileContent = await fileContentPromise;
+    let fileContent: string = await fileContentPromise as string;
+    return { fileContent, outputZipPath };
+}
 
-    /** database entry **/
-    let responseData : schema.Package;
-    if (URL) {
-        try {
-            const [results] = await connection.execute(
-                'INSERT INTO packages (Name, Version, ID, URL, JSProgram, Content) VALUES (?, ?, ?, ?, ?, ?)'
-                , [name, version, id, URL, JSProgram, fileContent]);
-            responseData = {
-                metadata: { Name: name, Version: version, ID: id.toString() },
-                data: { URL, JSProgram }
-            };
+async function decodeBase64AndExtract(base64String: string, outputPath: string) {
+    const cleanedBase64String = base64String.replace(/\s/g, '');
 
-        } catch (error) {
-            return res.status(500).json({
-                error: `Failed to insert URL to the database. Please try again later. ${error}`
-            });
-        }
+    // Create an instance of AdmZip using the base64 data
+    const zip = new AdmZip(Buffer.from(cleanedBase64String, 'base64'));
 
-    } else if (Content) {
+    // Extract the contents of the zip file directly to the output path
+    zip.extractAllTo(outputPath, /*overwrite*/ true);
 
+    // Get the list of entries in the output path
+    const outputEntries = fileSystem.readdirSync(outputPath);
+
+    // Determine the source and destination paths
+    const sourcePath = pathModule.join(outputPath, outputEntries[0]);
+    const destinationPath = pathModule.join(outputPath, 'contents');
+
+    // Move the contents to 'rest_api/dump/contents'
+    await fs.rename(sourcePath, destinationPath);
+
+    // Move the contents from 'rest_api/dump/contents' to 'rest_api/dump'
+    const contentsEntries = fileSystem.readdirSync(destinationPath);
+    for (const entry of contentsEntries) {
+        const entryPath = pathModule.join(destinationPath, entry);
+        const destinationEntryPath = pathModule.join(outputPath, entry);
+        await fs.rename(entryPath, destinationEntryPath);
     }
 
-    if (fileSystem.existsSync('rest_api/dump')) {
-        fileSystem.rmSync('rest_api/dump', { recursive: true });
-    }
-    if (fileSystem.existsSync(outputZipPath)) {
-        fileSystem.rmSync(outputZipPath, { recursive: true });
-    }
-    await connection.end();
-    return res.status(201).json(responseData);
-});
+    // Remove the intermediate 'contents' folder
+    await fs.rmdir(destinationPath);
 
-async function downloadRepo(url: string)  {
-
-    /** Create 'dump' directory if it doesn't exist **/
-    await fs.mkdir('rest_api/dump').catch(err => {
-        if (err.code !== 'EEXIST') {
-            console.error(`Error creating directory: ${err}`);
-        }
-    });
-
-    try {
-        await exec(`git clone ${url} rest_api/dump`);
-    } catch (error) {
-        throw error;
-    }
-
-    return path.join(`rest_api/dump`, 'package.json');
+    return outputPath; // Return the path of the final extracted contents in 'rest_api/dump'
 }
 
 export default router;
