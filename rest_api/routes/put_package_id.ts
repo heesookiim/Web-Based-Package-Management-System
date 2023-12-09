ddf4f7fb16a1fe6b20d9d9b85028897dfb1f1f6d
Andrewh18 <111818262+hall657@users.noreply.github.com>
2023-12-09 15:22:42 -0500

    fixed content handling and removed rating check

--- rest_api/routes/put_package_id.ts
+++ rest_api/routes/put_package_id.ts
@@ -52,28 +52,15 @@ async function updatePackage(packageId: PackageID, packageData: PackageData, pac
         logger.error(`Failed Database connection request in PUT. Error 500`);
         throw new Error('500')
     }
-    if (packageMetadata.ID != packageId) {
-        // Error handling for package ID mismatch
-        logger.error(`Package ID mismatch`);
-        throw new Error('404');
-    }
     try {
-        let packageRow;
-        try {
-            // Fetching the package with the specified ID from the database
-            logger.debug(`Fetching package with ID: ${packageId}`);
-            const [selectResults] = await connection.execute<RowDataPacket[]>(
-                `SELECT * FROM ${table} WHERE ID = ?`, [packageId]
-            );
+        // Fetching the package with the specified ID from the database
+        logger.debug(`Fetching package with ID: ${packageId}`);
+        const [selectResults] = await connection.execute<RowDataPacket[]>(
+            `SELECT * FROM ${table} WHERE ID = ?`, [packageId]
+        );
 
-            // Extracting the package row from the query results
-            packageRow = selectResults[0];
-        }
-        catch {
-            // Error handling for fetching the package
-            logger.error(`Error in fetching package with ID: ${packageId}`);
-            throw new Error('404');
-        }
+        // Extracting the package row from the query results
+        const packageRow = selectResults[0];
 
         // Log package details
         logger.debug(`package Name: ${packageMetadata.Name}`);
@@ -82,21 +69,29 @@ async function updatePackage(packageId: PackageID, packageData: PackageData, pac
         // Proceed if the package name and ID match the provided metadata
         if (packageRow.Name === packageMetadata.Name && packageRow.ID.toString() === packageId) {
             let return_data;
-            let url: any = '';
-            let content = '';
+            let url = packageData.URL || '';
             try {
                 // Handling package data based on URL or Content
                 if (packageData.URL) {
                     // Downloading the repository from the URL
-                    url = packageData.URL;
-                    logger.debug(`Downloading repo from URL: ${url}`);
-                    await downloadRepo(url, 'rest_api/dump');
+                    logger.debug(`Downloading repo from URL: ${packageData.URL}`);
+                    await downloadRepo(packageData.URL, 'rest_api/dump');
                 } else {
                     // Extracting the package content from Base64 encoded data
                     logger.debug('Decoding Base64 content and extracting');
-                    content = packageData.Content || '';
-                    // logger.debug(`${content}`);
-                    //await decodeBase64AndExtract(content, 'rest_api/dump');                    
+                    const content: any = packageData.Content;
+		    //logger.debug(`content: ${content}`);
+                    await decodeBase64AndExtract(content, 'rest_api/dump');
+
+                    // get github url from package.json
+                    logger.debug(`Extracting info from package.json`);
+                    const packageJsonPath = pathModule.join('rest_api/dump', 'package.json');
+                    const packageJsonContent = await fs.promises.readFile(packageJsonPath, 'utf-8');
+		    logger.debug(`JSONContent: ${packageJsonContent}`);
+                    const packageJson = JSON.parse(packageJsonContent);
+		    //logger.debug(`pacakgeJson: ${JSON.stringify(packageJson, null, 2)}`);
+                    url = getGithubUrl(packageJson.repository.url as string);
+		    logger.debug(`extracted URL: ${url}`);
                 }
                 logger.debug("Download complete");
             }
@@ -105,24 +100,6 @@ async function updatePackage(packageId: PackageID, packageData: PackageData, pac
                 logger.error(`Error in downloading repo from URL or decoding Base64 content: ${error}`);
                 throw new Error('500');
             }
-
-            if (content != ''){
-                try {
-                    // Extracting url from package uploaded by content
-                    logger.debug(`Extracting info from package.json for Content upload in PUT`);
-                    const packageJsonPath = pathModule.join('rest_api/dump', 'package.json')
-                    const packageJsonContent = await fs.promises.readFile(packageJsonPath, 'utf-8');
-                    const packageJson = JSON.parse(packageJsonContent);
-
-                    url = getGithubUrl(packageJson.repository.url as string);
-                }
-                catch {
-                    // Error handling for extracting url from package.json
-                    logger.error(`Error in extracting url from package.json in PUT`);
-                    throw new Error('500');
-                }
-            }
-
             try {
                 // Creating a ZIP file from the downloaded content
                 return_data = await ZIP('../dump', packageMetadata.Name, packageMetadata.Version, 'rest_api');
@@ -146,11 +123,11 @@ async function updatePackage(packageId: PackageID, packageData: PackageData, pac
             }
 
             // Checking if the package rating meets the specified criteria
-            if (packageRating && (packageRating.NetScore < 0.5 || packageRating.BusFactor < 0.5 || packageRating.Correctness < 0.5 || 
+            /*if (packageRating && (packageRating.NetScore < 0.5 || packageRating.BusFactor < 0.5 || packageRating.Correctness < 0.5 || 
                 packageRating.RampUp < 0.5 || packageRating.ResponsiveMaintainer < 0.5 || packageRating.LicenseScore < 0.5)) {
                 logger.error(`Package rating is invalid`);
                 throw new Error('424');
-            }
+            }*/
             
             try {
                 // Updating the package in the database
@@ -171,7 +148,7 @@ async function updatePackage(packageId: PackageID, packageData: PackageData, pac
                     WHERE ID = ?`,
                     [return_data.fileContent, 
                     packageMetadata.Version, 
-                    packageData.URL, 
+                    url, 
                     packageData.JSProgram,
                     packageRating.BusFactor,
                     packageRating.Correctness,
@@ -201,9 +178,9 @@ async function updatePackage(packageId: PackageID, packageData: PackageData, pac
             throw new Error('500');
         }
     } catch (error) {
-        // Handling other errors
-        logger.error(`Error in PUT request for package update: ${error}`);
-        throw error;
+        // Handling errors specific to fetching the package
+        logger.error(`404 error: ${error}`);
+        throw new Error('404');
     } finally {
         // Closing the database connection
         if (connection) {
@@ -225,7 +202,7 @@ router.put('/:id', async (req: Request, res: Response) => {
     // Checking for missing fields in the request
     logger.debug("Check for missing fields.");
 
-    if ((!packageData.URL && !packageData.Content) || (packageData.URL && packageData.Content) || !packageId || !packageData || !packageMetadata || !packageData.JSProgram || !packageMetadata.Name || !packageMetadata.Version || !packageMetadata.ID) {
+    if ((!packageData.URL && !packageData.Content) || !packageId || !packageData || !packageMetadata || !packageData.JSProgram || !packageMetadata.Name || !packageMetadata.Version || !packageMetadata.ID) {
         logger.error('Missing or improperly formed fields in PUT request');
         return res.status(400).json({ error: 'There is missing field(s) in the PackageID/AuthenticationToken or it is formed improperly, or the AuthenticationToken is invalid.' });
     }
@@ -233,29 +210,23 @@ router.put('/:id', async (req: Request, res: Response) => {
     logger.info(`Received PUT request for package update with ID: ${packageId}`);
     logger.info(`Received PUT request for package update with URL: ${packageData.URL}`);
     try {
-        if (packageData.URL != null) {
-            // Processing the URL for npm packages
-            let githubUrl: string | undefined = packageData.URL;
-            if (githubUrl && githubUrl.includes('npmjs.com')) {
-                try {
-                    let packageName = githubUrl.replace('https://www.npmjs.com/package/', '').split('/')[0];
-                    const npmData = await fetchNpmDataWithAxios(packageName);
-                    githubUrl = getGithubUrlFromNpmData(npmData) || undefined;
-                } catch (error) {
-                    logger.error('Failed to fetch GitHub URL from NPMJS link: ' + githubUrl);
-                    return res.status(400).json({ error: 'There is missing field(s) in the PackageID/AuthenticationToken or it is formed improperly, or the AuthenticationToken is invalid.' });
-                }
+        // Processing the URL for npm packages
+        let githubUrl = packageData.URL;
+        if (githubUrl && githubUrl.includes('npmjs.com')) {
+            try {
+                let packageName = githubUrl.replace('https://www.npmjs.com/package/', '').split('/')[0];
+                const npmData = await fetchNpmDataWithAxios(packageName);
+                githubUrl = getGithubUrlFromNpmData(npmData) || undefined;
+            } catch (error) {
+                logger.error('Failed to fetch GitHub URL from NPMJS link: ' + githubUrl);
+                return res.status(400).json({ error: 'There is missing field(s) in the PackageID/AuthenticationToken or it is formed improperly, or the AuthenticationToken is invalid.' });
             }
-            logger.debug(`PUT Input URL after conversion: ${githubUrl}`);
-
-            // Updating the package using the provided data
-            await updatePackage(packageId, { ...packageData, URL: githubUrl }, packageMetadata);
-        }
-        else {
-            // Updating the package using the provided data
-            await updatePackage(packageId, packageData, packageMetadata);
         }
 
+        logger.debug(`PUT Input URL after conversion: ${githubUrl}`);
+
+        // Updating the package using the provided data
+        await updatePackage(packageId, { ...packageData, URL: githubUrl }, packageMetadata);
         res.status(200).json({ message: 'Version is updated.' });
     } catch (error: any) {
         // Handling different types of errors during the PUT request
@@ -264,8 +235,8 @@ router.put('/:id', async (req: Request, res: Response) => {
             res.status(404).json({ error: 'Package does not exist.' });
         } else if (error.message == '400') {
             res.status(400).json({ error: 'There is missing field(s) in the PackageID/AuthenticationToken or it is formed improperly, or the AuthenticationToken is invalid.' });
-        } else if (error.message == '424') {
-            res.status(424).json({ error: 'Package is not uploaded due to the disqualified rating.' });
+        //} else if (error.message == '424') {
+        //    res.status(424).json({ error: 'Package is not uploaded due to the disqualified rating.' });
         } else {
             res.status(500).json({ error: 'Internal Server Error.' });
         }
